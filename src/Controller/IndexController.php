b9747de13bd49d5f19fdc9da4dbc4827e20063c2
Vladimir Teplov <vteplov@brightgrove.com>
2021-05-20 16:14:05 +0300

    view: return json instead of rendering template

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,8 +13,6 @@ class IndexController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        return $this->json(['data' => 'Success!']);
     }
 }
